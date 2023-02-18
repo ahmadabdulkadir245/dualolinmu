d41214f8822edb294571d5471db24168dded0453
unknown <ahmadabdulkadir245@gmail.com>
2023-02-17 18:42:03 -0800

    remove version env

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -2,11 +2,10 @@ import {createClient} from 'next-sanity'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
- const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 
  export const client = createClient({
     projectId,
     dataset,
-     apiVersion,
+     apiVersion:  "2022-11-15",
     useCdn: false
  }) 
