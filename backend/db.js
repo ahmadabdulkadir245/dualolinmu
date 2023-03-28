cfd0b45f305922574a6140d190794b0835fcd7f3
unknown <ahmadabdulkadir245@gmail.com>
2023-03-27 18:53:47 -0700

    added connection limit and other things to db config

--- backend/db.js
+++ backend/db.js
@@ -4,6 +4,9 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password:  process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 })
 module.exports = db
