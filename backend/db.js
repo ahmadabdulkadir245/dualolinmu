b94505d12d63ab548d0fd171b09a0da3d8def26d
unknown <ahmadabdulkadir245@gmail.com>
2023-03-28 01:47:01 -0700

    revome timeout to fix backend bug

--- backend/db.js
+++ backend/db.js
@@ -5,8 +5,8 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password:  process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0
 })
 module.exports = db
